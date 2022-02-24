9b27d0cdc4e77eab7b1c1084ce25fe305bcb71d7
Alla <allakakhanovich@gmail.com>
2022-02-24 23:42:58 +0300

    fix: change version node

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if(!matrix) {
+    return [];
+  }
+
+  for(el of matrix) {
+    el.sort((a, b) => a-b);
+  }
+  matrix = matrix.reduce((acc, cur, i) => {
+    return acc.concat((!(i % 2) ? cur : cur.reverse()));
+  }, []);
+  return matrix;
 }
